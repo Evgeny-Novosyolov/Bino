b52583f1a8acd5673effa965f8d10ecb57b71200
Evgeny Novosyolov <Evgeny.Novosyolov.job@yandex.ru>
2019-06-10 09:45:33 +0300

    js

--- src/blocks/recent/recent.js
+++ src/blocks/recent/recent.js
@@ -1,25 +1,22 @@
 $(document).ready(function () {
 
-  var $recent = [, "[data-name='print']", "[data-name='animation']", "[data-name='art']", "[data-name='web']", "[data-name='photography']", "[data-name='video']"];
+  var $recentArr = ["all", "[data-name='print']", "[data-name='animation']", "[data-name='art']", "[data-name='web']", "[data-name='photography']", "[data-name='video']"];
 
-  var resentShow = function (num, recetItem) {
-    $(".recent__slider-item").eq(num).on("click", function () {
-      $(this)
-      $(".recent__slider-box").show(500);
-      $(".recent__slider-box").not(recetItem).hide();
-      if (num == 0) {
-        $(".recent__slider-box").show(500);
-      }
-    });
-
-  };
-
-  resentShow(0, $recent[0]);
-  resentShow(1, $recent[1]);
-  resentShow(2, $recent[2]);
-  resentShow(3, $recent[3]);
-  resentShow(4, $recent[4]);
-  resentShow(5, $recent[5]);
-  resentShow(6, $recent[6]);
-
+  function resentShow() {
+    let indexItem = 0;
+    for (let i = 0; i < $recentArr.length; i++) {
+      $recentArr.forEach((item) => {
+        $(".recent__slider-item").eq(indexItem).on("click", function () {
+          if (item == "all") {
+            $(".recent__slider-box").hide();
+            $(".recent__slider-box").show(500);
+          }
+          $(".recent__slider-box").show(500);
+          $(".recent__slider-box").not(item).hide();
+        })
+        indexItem++
+      })
+    }
+  }
+  resentShow()
 });
--- src/js/imports/blocks.js
+++ src/js/imports/blocks.js
@@ -1,40 +1,38 @@
 $(document).ready(function () {
 
- var scroollBox = function (a, b) {
-    let boxParent = a;
-    let boxChild = b;
+  var $boxShowArr = [".features__block", ".history__box", ".services__box--left", ".study-main__box", ".catalog__box", ".pricing-main__block", ".touch__box", ".blog__block"];
 
-    $(window).scroll(function () {
-      if ($(boxParent).offset().top <= $(window).scrollTop() + 700) {
-        $(boxChild).addClass("jackInTheBox");
-        
-      }
-    })
+
+  function scroollBox() {
+    for (let i = 0; i < $boxShowArr.length; i++) {
+      $(window).scroll(function () {
+       $boxShowArr.forEach((item) => {
+          if ($(item).offset().top <= $(window).scrollTop() + 700) {
+            $($(item)).addClass("jackInTheBox");
+          }
+        });
+      });
+    }
   }
- 
-  var $section = {
-    features: [".features-wrapper", ".features__block"],
-    history: [".history-wrapper", ".history__box"],
-    services: [".services-wrapper", ".services__box--left"],
-    study: [".study-wrapper", ".study-main__box"],
-    catalog: [".catalog__block", ".catalog__box"],
-    pricing: [".pricing-main__block", ".pricing-main__block"],
-    touch: [".touch__block", ".touch__box"],
-    blog: [".blog-container", ".blog__block"]
-  }
+  scroollBox();
+
+  $(".span-scroll").on("click", function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
+
+  // for (let i = 0; i < arrBoxShow.length; i++) {
+  //   divTitle[i].onclick = function () {
+  //     divText.forEach((item) => item.classList.remove('device-list__text--open'));
+  //     divText[i].classList.add('device-list__text--open');
+  //   };
+  // }
 
 
 
-  scroollBox($section.features[0], $section.features[1]);
-  scroollBox($section.history[0], $section.history[1]);
-  scroollBox($section.services[0], $section.services[1]);
-  scroollBox($section.study[0], $section.study[1]);
-  scroollBox($section.catalog[0], $section.catalog[1]);
-  scroollBox($section.pricing[0], $section.pricing[1]);
-  scroollBox($section.touch[0], $section.touch[1]);
-  scroollBox($section.blog[0], $section.blog[1]);
-
-  
   // class Rectangle {
   //   constructor(boxChild, boxParent) {
   //     this.boxChild = boxChild;
@@ -67,12 +65,6 @@ $(document).ready(function () {
 
 
 
-  $(".span-scroll").on("click", function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 500);
-    return false;
-  })
 
 
 
